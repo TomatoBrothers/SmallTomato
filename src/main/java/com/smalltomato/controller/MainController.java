8bbba02737c915182832acd47075544c1da9a989
wangshichong <458250970@qq.com>
2017-11-30 01:15:53 +0800

    添加登录界面
    添加登录界面

---
+++ src/main/java/com/smalltomato/controller/MainController.java
@@ -0,0 +1,20 @@
+package com.smalltomato.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author WangShiChong
+ * @ Create Time: 2017/11/28/0028 23:10
+ */
+@Controller
+@RequestMapping("main")
+public class MainController {
+
+    @RequestMapping("/login")
+    public String login() {
+        System.out.println("程序到此一游！");
+        return "/login";
+    }
+
+}
