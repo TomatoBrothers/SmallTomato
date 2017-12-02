b35ea0cf51a117f230dc05466a50b7b14ac745e2
451462664 <30548091+451462664@users.noreply.github.com>
2017-12-02 21:08:11 +0800

    update
    update

--- src/main/java/com/smalltomato/dao/DepartmentMapper.java
+++ src/main/java/com/smalltomato/dao/DepartmentMapper.java
@@ -4,10 +4,6 @@ import com.smalltomato.base.dao.BaseDao;
 import com.smalltomato.bean.Department;
 import com.smalltomato.bean.DepartmentExample;
 
-import java.util.List;
-
-import org.apache.ibatis.annotations.Param;
-
 public interface DepartmentMapper extends BaseDao<Department, DepartmentExample, Integer> {
 
 }
